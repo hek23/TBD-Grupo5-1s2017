4a4b5e730576df7bd919028ce104735b0cd9fda9
Héctor Fuentealba <hek23@users.noreply.github.com>
2017-04-25 18:53:16 -0300

    Fix Instalacion MongoDB (2.6 -> 3.4)

--- InstalacionRequerimientosServer.sh
+++ InstalacionRequerimientosServer.sh
@@ -27,7 +27,7 @@ rm glassfish-4.0.zip
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get -y update
-sudo apt-get -y install mongodb
+sudo apt-get install -y mongodb-org
 wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip
 unzip gradle-3.4.1-bin.zip -d /usr/local
 cd /usr/local
