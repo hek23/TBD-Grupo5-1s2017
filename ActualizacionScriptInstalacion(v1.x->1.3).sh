680cd13b2d4829e6f85eefd9af0c05f97a2f3c7d
hek23 <hector.fuentealba@usach.cl>
2017-05-02 13:53:41 -0300

    Actualizado automatización de instalador

--- ActualizacionScriptInstalacion(v1.x->1.3).sh
+++ ActualizacionScriptInstalacion(v1.x->1.3).sh
@@ -1,11 +1,11 @@
-sudo apt-get autoremove mongo
+sudo apt-get autoremove -y mongo
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get -y update
 sudo apt-get install -y mongodb-org
 #SERVIDOR NGINX PARA REDIRECCION Y PUERTOS
 sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
 sudo systemctl stop nginx
 sudo systemctl start nginx
